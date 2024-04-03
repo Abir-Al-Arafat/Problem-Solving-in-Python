2a5d27b0a9b586eb72a556facafcd617fe7c760b
Abir-Al-Arafat <antorabir@gmail.com>
2024-04-04 02:13:53 +0600

    new problems added

--- Leetcode/tempCodeRunnerFile.py
+++ Leetcode/tempCodeRunnerFile.py
@@ -1,22 +1,3 @@
-for idx in range(1, len(input)):
-    if maxLeft[idx-1] < input[idx]:
-        maxLeft.append(biggestLeft)
-        biggestLeft = input[idx]
-    else:
-        maxLeft.append(biggestLeft)
-
-for idx in range(len(input)-2, 0, -1):
-    print("input[idx+1] input[idx]")
-    print(input[idx+1], "-------------",input[idx])
-    if biggestRight < input[idx]:
-        # if len(maxRight) == 0:
-        #     maxRight.append(biggestRight)
-        # else:
-        maxRight.insert(0, biggestRight)
-        # [biggestRight] + maxRight
-        biggestRight = input[idx]
-        print("input[idx] bigger", maxRight)
-    else:
-        maxRight.insert(0, biggestRight)
-        print("input[idx]", maxRight)
-        # [biggestRight] + maxRight
+print("frequency", frequency)
+            # print("result", result)
+            # print("right-left+1", right-left+1)
